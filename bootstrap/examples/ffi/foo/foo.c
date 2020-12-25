b0925a527b26c13272f2f14c94e4f365b25aa195
Sameer Rahmani <lxsameer@gnu.org>
2020-12-25 21:57:13 +0000

    [Bootstrap] Add a sample shared library to used with ffi api

---
+++ bootstrap/examples/ffi/foo/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+
+void bar(void)
+{
+    puts("Hello, I am a shared library");
+}
---
+++ bootstrap/examples/ffi/foo/foo.h
@@ -0,0 +1,6 @@
+#ifndef foo_h__
+#define foo_h__
+
+extern void baz(void);
+
+#endif  // foo_h__
