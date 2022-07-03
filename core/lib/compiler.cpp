e2873780b89f8d057e104e6a3f6ea138374f2df4
Sameer Rahmani <lxsameer@gnu.org>
2022-07-03 11:24:01 +0100

    Add two basic modules to core in order to test ir unification of clang

--- core/lib/compiler.cpp
+++ core/lib/compiler.cpp
@@ -17,11 +17,10 @@
  */
 
 #include "serene/core/reader.h"
-
 #include "serene/types/types.h"
 
 namespace serene {
 
-int reader() { return 0; };
+int compile() { return 0; };
 
 } // namespace serene
